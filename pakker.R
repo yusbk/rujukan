3a168dcf7dc43034f8f6688bf5632637070ada67
Yusman BK <ybkamaleri@gmail.com>
2018-07-25 17:16:26 +0200

    Standard R pakker

---
+++ pakker.R
@@ -0,0 +1,12 @@
+## Pakker som jeg trenger hvergang ny installasjon skal gjenomføres.
+## Disse skal installeres om det ikke allerede gjort i folder "r-pakker"
+
+load <- function(pkg){
+  nypkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(nypkg))
+    install.packages(nypkg, dependencies = TRUE, repos = "http://cran.r-project.org")
+  sapply(pkg, require, character.only = TRUE)
+}
+
+pakker <- c("knitr", "data.table", "ggplot2", "shiny", "shinydashboard")
+load(pakker)
